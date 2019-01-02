e36ce405b4cb921b704b23cc487e27efac6b618d
satokibi <td182007@cis.fukuoka-u.ac.jp>
2019-01-02 18:52:39 +0900

    skillsetの作成

--- src/views/About.vue
+++ src/views/About.vue
@@ -75,7 +75,7 @@
                   .level-left
                     span.icon.has-text-dark
                       i.far.fa-envelope
-                    p.prof-title mail
+                    p.me-title mail
                   .level-right
                     .
                       <span class="icon">
@@ -87,7 +87,7 @@
                   .level-left
                     span.icon.has-text-light
                       i.fab.fa-github
-                    p.prof-title.has-text-light GitHub
+                    p.me-title.has-text-light GitHub
                   .level-right
                     .
                       <span class="icon has-text-light">
@@ -102,7 +102,7 @@
                         <i class="fa fa-square fa-stack-1x"></i>
                         <i class="fa fa-search fa-stack-1x fa-inverse fa-1x"></i>
                       </span>
-                    p.prof-title.has-text-light Qiita
+                    p.me-title.has-text-light Qiita
                   .level-right
                     .
                       <span class="icon has-text-light">
@@ -113,9 +113,75 @@
       .card.has-text-left.has-background-light
         header.card-header.has-background-dark
           p.card-header-title.has-text-light SkillSet
-        .card-content aiueo
+        .card-content 
+          div.has-text-centered.skill-content
+            div.skill-title.is-inline-block
+              .
+                <span class="icon" style="color: #de4b26;">
+                  <i class="fab fa-html5"></i>
+                </span>
+              p.is-inline HTML
+            div.skills
+              p.is-inline-block <span><img src="../assets/pug.png"></span> pug
+              p.is-inline-block <span><img src="../assets/emmet.svg"></span> emmet
 
-        
+          div.has-text-centered.skill-content
+            div.skill-title.is-inline-block
+              .
+                <span class="icon" style="color: #2196f3;">
+                  <i class="fab fa-css3"></i>
+                </span>
+              p.is-inline CSS
+            div.skills
+              p.is-inline-block
+                span.icon.scss-icon
+                  i.fab.fa-sass
+                | Scss
+              p.is-inline-block <span><img src="../assets/bootstrap.png"></span> Bootstrap
+              p.is-inline-block <span><img src="../assets/bulma.png"></span> Bulma
+
+          div.has-text-centered.skill-content
+            div.skill-title.is-inline-block
+              .
+                <span class="icon" style="color: #ead44d;">
+                  <i class="fab fa-js-square"></i>
+                </span>
+              p.is-inline JavaScript
+            div.skills
+              p.is-inline-block <span><img src="../assets/jquery.png"></span> jQuery
+              p.is-inline-block <span><img src="../assets/vue.png"></span> Vue.js
+
+          div.has-text-centered.skill-content
+            div.skill-title.is-inline-block
+              .
+                <img src="https://img.icons8.com/color/50/000000/python.png">
+              p.is-inline Python
+            div.skills
+              p.is-inline-block <span><img src="../assets/scikit-learn.png" style="width: 32px; top: 2px;"></span> scikit-learn
+              p.is-inline-block <span><img src="../assets/caffe.png"></span> Caffe
+
+          div.has-text-centered.skill-content
+            div.skill-title.is-inline-block
+              .
+                <img src="../assets/java.png">
+              p.is-inline Java
+            div.skills
+              p.is-inline-block <span><img src="../assets/play.png"></span> PlayFramework
+
+          div.has-text-centered.skill-content.skill-content-last
+            div.skill-title.is-inline-block
+              .
+                <span class="icon" style="color: ;">
+                  <i class="fas fa-wrench"></i>
+                </span>
+              p.is-inline Tools
+            div.skills
+              p.is-inline-block <span><img src="../assets/vim.png"></span> vim
+              p.is-inline-block <span><img src="https://img.icons8.com/color/48/000000/git.png"></span> Git
+              p.is-inline-block <span><img src="../assets/docker.png"></span> Docker
+              p.is-inline-block <span><img src="../assets/tmux.png"></span> tmux
+              p.is-inline-block <span><img src="../assets/unity.png"></span> Unity
+
 </template>
 
 <style lang="scss" scoped>
@@ -164,8 +230,51 @@
   .me-btn {
     padding: 8px 16px;
     margin-bottom: 8px;
+
+    .me-title {
+      margin-left: 16px;
+    }
   }
 
+  .skill-title {
+    padding-right: 4px;
+
+    img {
+      position: relative;
+      top: 2px;
+      width: 16px;
+    }
+    p {
+      margin-left: 4px;
+    }
+    border-bottom: 1px solid #aaa;
+  }
+
+  .skill-content {
+    margin-bottom: 28px;
+    .skills {
+      margin-top: 8px;
+      p {
+        margin: 0 12px;
+        font-size: 12px;
+      }
+      img {
+        width: 16px;
+        position: relative;
+        top: 4px;
+      }
+
+      .scss-icon {
+        color: #cf649a;
+      }
+
+    }
+
+  }
+
+  .skill-content-last {
+    margin-bottom: 0px;
+  }
 
 }
 </style>
