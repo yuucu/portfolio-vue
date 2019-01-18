8391d00b6bf8c4048b7ac269c855f2210849fd62
satokibi <td182007@cis.fukuoka-u.ac.jp>
2019-01-18 09:12:31 +0900

    add terminal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template lang="pug">
   .home
-    p サイト更新中...
-    p 前サイト
-    a( href="https://satokibi.github.io/2/" ) https://satokibi.github.io/2/
+    .terminal
+      p サイト更新中...
+      p 前サイト
+      a( href="https://satokibi.github.io/2/" ) https://satokibi.github.io/2/
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style lang="scss" scoped>
+.terminal {
+  width: 420px;
+  height: 280px;
+  margin: 0 auto;
+  margin-top: 104px;
+  margin-bottom: 120px;
+  background-color: black;
+}
+</style>
