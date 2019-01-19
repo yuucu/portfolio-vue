073eba74a6e222c5abd7632c712cea89b78c9b55
satokibi <td182007@cis.fukuoka-u.ac.jp>
2019-01-19 10:01:30 +0900

    terminal login function

--- src/components/Home/home_terminal.vue
+++ src/components/Home/home_terminal.vue
@@ -1,10 +1,20 @@
 <template lang="pug">
-  .terminal
+  .terminal( v-on:click="terminalFocus" )
     ul
       li( v-for=" (text, index) in history " )
-        p [~]# {{ text }}
-      li [~]#&nbsp;
-        input( v-model="current_line" v-on:keydown.enter="command")
+        p {{ text }}
+      li( v-if="login_flag" ) [~]#&nbsp;
+        input( autofocus maxlength="50" id="terminal-input" v-model="current_line" v-on:keydown.enter="command")
+      li( v-else )
+        div( v-if="login_state_username" )
+          | login:&nbsp;
+          input( autofocus maxlength="50" id="terminal-input" v-model="username" class="input_username"
+          v-on:keydown.enter="login")
+        div( v-else )
+          | password:&nbsp;
+          input( autofocus type="password" maxlength="32"
+          id="terminal-input" v-model="password" class="input_password"
+          v-on:keydown.enter="login")
 </template>
 
 <script>
@@ -14,16 +24,78 @@ export default {
   data: function() {
     return {
       current_line: "",
-      history: ['Hello, World!']
+      username: "",
+      password: "",
+      history: [],
+      index: 0,
+      interval: 500,
+      login_flag: false,
+      login_state_username: true,
+      terminal_init_messages: [
+        'Initializing ...',
+        'site version 0.11 2019 Saturday 19 January 08:35 a.m. JST',
+        window.navigator.language,
+        window.navigator.userAgent,
+      ],
+      login_messages: [
+        '　',
+        'site ver 0.11',
+        'Welcome',
+        '　',
+      ]
     }
   },
+  created: function() {
+    // setInterval(this.terminalInit , this.interval)
+    this.terminalInit();
+  },
   methods: {
+    login: function() {
+      if( this.login_state_username ) {
+        this.login_state_username = false;
+        this.history_push( 'login: ' + this.username );
+      } else {
+        this.login_state_username = true;
+        this.history_push( 'password: ' + '*'.repeat( this.password.length ) );
+
+        if( this.username === 'a' && this.password === 'a') {
+          this.history_push( 'Login correct' );
+          this.login_flag = true;
+        } else {
+          this.history_push( 'Login incorrect' );
+          this.history_push( '　' );
+          this.username = '';
+          this.password = '';
+        }
+
+      }
+    },
     command: function() {
-      this.history.push( this.current_line );
+      this.history_push( '[~]# ' + this.current_line );
+      this.current_line = '';
+    },
+    terminalFocus: function() {
+      document.getElementById('terminal-input').focus();
+    },
+    terminalInit: function() {
+      this.history_push( this.terminal_init_messages[this.index] );
+      this.current_line = '';
+      this.interval = Math.random() * 100;
+      this.index ++;
+      if( this.index < this.terminal_init_messages.length ) {
+        setTimeout( this.terminalInit , this.interval)
+      } else {
+        this.history = [];
+        for( var i = 0; i < this.login_messages.length; i++ ) {
+          this.history_push( this.login_messages[i] );
+        }
+      }
+    },
+    history_push: function( str ) {
+      this.history.push( str );
       if( this.history.length > 14 ) {
         this.history = this.history.slice( -14 );
       }
-      this.current_line = '';
     }
   }
 }
@@ -36,7 +108,7 @@ export default {
 .terminal {
   font-family: 'Source Code Pro', monospace;
   overflow: hidden;
-  width: 420px;
+  width: 404px;
   height: 272px;
   margin: 0 auto;
   margin-top: 104px;
@@ -47,6 +119,13 @@ export default {
   font-size: 12px;
   color: red;
 
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
   ul {
     li {
       color: white;
@@ -58,11 +137,19 @@ export default {
     background-color: black;
     color: white;
     border: none;
-  font-family: 'Source Code Pro', monospace;
-  font-size: 12px;
+    font-family: 'Source Code Pro', monospace;
+    font-size: 12px;
+    width: 364px;
 
     &:focus {
       outline: 0;
+    }
+
+    &.input_username  {
+      width: 340px;
+    }
+    &.input_password  {
+      width: 308px;
     }
   }
 }
