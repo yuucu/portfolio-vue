a5a87885478b2a248ed44b3e677e7963d02ac6b4
satokibi <td182007@cis.fukuoka-u.ac.jp>
2019-02-10 05:20:43 +0900

    fix works

---
+++ src/components/Works/item/rettiwt.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  .column.is-4-tablet.is-3-widescreen
+    .card
+      .card-image
+        a( href="https://satokibi.github.io/rettiwt/" target="_blank" )
+          img( src="@/assets/rettiwt.png" class="image")
+        div.content
+          div.tags
+            span( class="icon" style="color: #de4b26;" )
+              i( class="fab fa-html5" )
+            span( class="icon" style="color: #2196f3;" )
+              i( class="fab fa-css3" )
+            span( class="icon" style="color: #ead44d;" )
+              i( class="fab fa-js-square" )
+            span
+              a( href="https://getbootstrap.com/" target="_blank" )
+                img( src="@/assets/bootstrap.png" )
+          p.title-text rettiwt
+          p.description いろいろ文字変換できるサイト。leet文字変換や、文字を回転したり、変な記号をたくさんつけたり、、、
+          div.link
+            a( href="https://github.com/satokibi/rettiwt" target="_blank" )
+              i( class="fab fa-github" )
+            a( href="https://satokibi.github.io/rettiwt/" target="_blank" )
+              i( class="fas fa-external-link-alt" )
+</template>
